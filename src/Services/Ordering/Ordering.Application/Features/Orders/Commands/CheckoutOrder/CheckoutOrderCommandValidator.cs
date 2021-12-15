c7891098d0317ec9763be0eb91ff2d64dcf98297
MouradM <wassakh@gmail.com>
2021-12-15 18:06:35 +0100

    Order API CQRS

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+	public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+	{
+		public CheckoutOrderCommandValidator()
+		{
+			RuleFor(a => a.UserName)
+				.NotEmpty()
+				.WithMessage("{UserName} is required.")
+				.MaximumLength(50)
+				.WithMessage("{UserName} must not exceed 50 characters.");
+
+			RuleFor(a => a.EmailAddress)
+				.NotEmpty()
+				.WithMessage("{EmailAddress} is required.");
+
+			RuleFor(a => a.TotalPrice)
+				.NotEmpty()
+				.WithMessage("{TotalPrice} is required.")
+				.GreaterThan(0)
+				.WithMessage("{TotalPrice} should be greater than 0.");			;
+		}
+
+		
+	}
+}
