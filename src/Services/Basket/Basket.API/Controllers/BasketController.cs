eab36fc51eaf66167cbde352f3b4cdf92407916b
MouradM <wassakh@gmail.com>
2021-12-19 20:53:04 +0100

    docker rabbitmq

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -5,6 +5,7 @@ using Basket.API.Repositories;
 using EventBus.Messages.Events;
 using MassTransit;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,13 +22,15 @@ namespace Basket.API.Controllers
         private readonly DiscountGrpcService _discountGrpcService;
         private readonly IMapper _mapper;
         private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<BasketController> _logger;
 
-		public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IMapper mapper, IPublishEndpoint publishEndpoint)
+		public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IMapper mapper, IPublishEndpoint publishEndpoint, ILogger<BasketController> logger)
 		{
 			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
 			_discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+			_mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+			_publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
 		[HttpGet("{userName}", Name = "GetBasket")]
@@ -73,20 +76,30 @@ namespace Basket.API.Controllers
             // send checkout event to rabbitmq
             // remove the basket
 
+            _logger.LogInformation("Checkout for basketckeckout userName {userName}", basketCheckout.UserName);
+
             // get existing basket with total price
             var basket = await _repository.GetBasket(basketCheckout.UserName);
             if (basket == null)
             {
+                _logger.LogError("Basket not found for userName {userName}", basketCheckout.UserName);
                 return BadRequest();
             }
 
             // send checkout event to rabbitmq
             var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
             eventMessage.TotalPrice = basket.TotalPrice;
+
+            _logger.LogInformation("Calculating TotalPrice: {totalPrice}", basket.TotalPrice);
+
             await _publishEndpoint.Publish(eventMessage);
+
+            _logger.LogInformation("BasketCheckoutEvent published", basket.TotalPrice);
 
             // remove the basket
             await _repository.DeleteBasket(basket.UserName);
+
+            _logger.LogInformation("Basket deleted for userName {userName}", basket.UserName);
 
             return Accepted();
         }
